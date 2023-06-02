2f62bdfc4be4db222de745c134c60bce2297c563
Milltiades <gigakakulia2017@gmail.com>
2023-06-03 02:56:22 +0400

    browser router replaced with hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { ParallaxProvider } from "react-scroll-parallax";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import {  HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ParallaxProvider>
         <App />
       </ParallaxProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
